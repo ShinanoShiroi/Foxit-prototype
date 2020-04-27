57fd18c349cf7e98bf4c8c144db58ed2b7b0f68d
s1lentp4nther <64418638+s1lentp4nther@users.noreply.github.com>
2020-04-27 16:05:42 +0200

    Add files via upload

---
+++ KillEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemy : MonoBehaviour
+
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            Destroy(this.gameObject);
+    }
+}
