641c399f4fb883a9c35f1b4bad3d54b4abbd7e84
Damiano Leo <64418638+s1lentp4nther@users.noreply.github.com>
2020-04-28 20:31:53 +0200

    Add files via upload

---
+++ scripts/CoinScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            AudioManager.instance.Play("coin"); // plays coin sound
+            Destroy(gameObject);  // Destroys things
+        }
+    }
+}
---
+++ scripts/KillPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            Application.LoadLevel(Application.loadedLevel);
+    }
+}
